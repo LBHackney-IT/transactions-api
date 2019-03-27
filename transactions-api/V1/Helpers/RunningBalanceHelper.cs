83850336a8a810f25a6b2f01b48660deccc478cb
LBHMGeorgieva <mirela.georgieva@hackney.gov.uk>
2019-03-27 15:47:06 +0000

    added initial draft of calculation

---
+++ transactions-api/V1/Helpers/RunningBalanceHelper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using transactions_api.V1.Domain;
+
+namespace transactions_api.V1.Helpers
+{
+    public class RunningBalanceHelper
+    {
+        public static List<Transaction> CalculateRunningBalance(List<Transaction> listOfTransactions)
+        {
+            //running balance starts from 0
+            decimal runningBalance = 0;
+            foreach (var transaction in listOfTransactions)
+            {
+                runningBalance = transaction.Amount + runningBalance;
+                transaction.RunningBalance = runningBalance;
+            }
+
+            return listOfTransactions;
+        }
+    }
+}
