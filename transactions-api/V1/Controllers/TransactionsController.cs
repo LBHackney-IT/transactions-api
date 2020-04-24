812ab5504e8b894ec263bb604d073e1dbb8a47cc
Liudvikas Taluntis <liudvikast@gmail.com>
2020-04-24 13:43:27 +0100

    Added error handling and few annotations for Swagger page.
    Nothing here has any tests as a sacrifice for speed as required.

--- transactions-api/V1/Controllers/TransactionsController.cs
+++ transactions-api/V1/Controllers/TransactionsController.cs
@@ -34,22 +34,46 @@ namespace transactions_api.Controllers.V1
         }
 
         [HttpGet]
-        [Route("payment-ref/{payment_ref}/post-code/{post_code}")] //should we add "GetAllTenancyTransactions/" to the start of the url?
+        [Route("payment-ref/{payment_ref}/post-code/{post_code}")]                                              //should we add "GetAllTenancyTransactions/" to the start of the url?
         [Produces("application/json")]
+        [ProducesResponseType(typeof(GetAllTenancyTransactionsResponse), 200)]
+        [ProducesResponseType(typeof(ErrorResponse), 400)]
+        [ProducesResponseType(typeof(ErrorResponse), 500)]
         public IActionResult GetAllTenancyTransactions([FromRoute] GetAllTenancyTransactionsRequest request)
         {
-            var validationResult = _getTenancyTransactionsValidator.Validate(request);
-            
-            if (validationResult.IsValid)
-            {
-                var usecaseResponse = _listTransactions.ExecuteGetTenancyTransactions(request);
-
-                return Ok(usecaseResponse);
-            }
-
-            return BadRequest(
-                    new ErrorResponse(validationResult.Errors)
+            _logger.LogInformation(                                                                             //TODO: Add tests for logging! An the rest of the logging. Add logging message formatter.
+                $"The request has hit GetAllTenancyTransactions controller with the following data. PaymentRef = {request.PaymentRef ?? "null" } and PostCode = {request.PostCode ?? "null"}"
                 );
+
+            try                                                                                                 //TODO: add tests for this. No tests due to this needing to be rushed!
+            {
+                var validationResult = _getTenancyTransactionsValidator.Validate(request);
+
+                if (validationResult.IsValid)
+                {
+                    var usecaseResponse = _listTransactions.ExecuteGetTenancyTransactions(request);
+
+                    return Ok(usecaseResponse);
+                }
+
+                return BadRequest(
+                    new ErrorResponse(validationResult.Errors)
+                    );
+            }
+            catch (AggregateException ex) when ( ex.InnerException != null )
+            {
+                return StatusCode(
+                    500,
+                    new ErrorResponse(ex.Message, ex.InnerException.Message)
+                    );
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    500,
+                    new ErrorResponse(ex.Message)
+                    );
+            }
         }
     }
 }
