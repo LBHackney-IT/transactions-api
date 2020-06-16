af3c50253bfff6c170168c1c4239ae4accfb04e2
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-06-16 11:19:52 +0100

    Changed type of the two dates to be DateTime because Dapper was doing odd things with the dates...

--- transactions-api/V1/Domain/TenancyTransaction.cs
+++ transactions-api/V1/Domain/TenancyTransaction.cs
@@ -7,7 +7,7 @@ namespace transactions_api.V1.Domain                            //This is where 
 {
     public class TenancyTransaction                             //'In' and 'Out' - don't think it makes sense to have it like this long term. I think it's Front-End's responsibility to distinguish between positive and negative amounts.
     {
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public string Description { get; set; }
         public string In { get; set; }
         public string Out { get; set; }
@@ -16,7 +16,7 @@ namespace transactions_api.V1.Domain                            //This is where 
 
     public class TempTenancyTransaction                         //Temporary class from NCC so that the data from sql query would get automapped. But I think this should be our return object with an addition of balance.
     {
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public string Amount { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
